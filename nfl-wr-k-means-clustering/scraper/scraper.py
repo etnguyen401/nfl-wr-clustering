6b8f91a39f6d05e02b1516bf79012725e5380fcc
etnguyen401 <nguyeneric280@gmail.com>
2026-05-04 15:02:31 -0700

    add code to get combine data for WRs

--- nfl-wr-k-means-clustering/scraper/scraper.py
+++ nfl-wr-k-means-clustering/scraper/scraper.py
@@ -1,1 +1,17 @@
+import os
 import pandas as pd
+import nflreadpy as nfl
+
+combine_data = None
+
+if (os.path.exists("data/combine_data_wr.csv")):
+    print("Loading combine data from CSV...")
+    combine_data = pd.read_csv("data/combine_data_wr.csv")
+else:
+    print("Scraping combine data from nflreadpy...")
+    combine_data = nfl.load_combine().to_pandas()
+    #filter for only wide receivers
+    combine_data = combine_data.loc[(combine_data["pos"] == "WR")]
+    combine_data.to_csv("data/combine_data_wr.csv", index=False)
+
+print(combine_data.head())
